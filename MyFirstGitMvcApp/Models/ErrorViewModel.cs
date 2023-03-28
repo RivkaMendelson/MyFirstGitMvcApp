1b37dcdf84583077f8fdcef98a93b3b3feb0c68a
Rivka Mendelson <rivkamendelson123@gmail.com>
2023-03-27 21:19:57 -0400

    Add project files.

---
+++ MyFirstGitMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFirstGitMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
